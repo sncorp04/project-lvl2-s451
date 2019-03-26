cdccbf8e0ccce7246541d63cf5b591d4f75c5275
sncorp <a.a.malutin@gmail.com>
2019-03-26 12:00:38 +0300

    step 3

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -1,18 +1,15 @@
 import fs from 'fs';
 import gendiff from '../src';
 
-test('gendiff json', () => {
-  const fileBefore = '__tests__/__fixtures__/before.json';
-  const fileAfter = '__tests__/__fixtures__/after.json';
-  const actual = gendiff(fileBefore, fileAfter);
-  const expected = fs.readFileSync('__tests__/__fixtures__/expected', 'utf-8').trim();
-  expect(actual).toBe(expected);
-});
-
-test('gendiff yaml', () => {
-  const fileBefore = '__tests__/__fixtures__/before.yml';
-  const fileAfter = '__tests__/__fixtures__/after.yml';
-  const actual = gendiff(fileBefore, fileAfter);
-  const expected = fs.readFileSync('__tests__/__fixtures__/expected', 'utf-8').trim();
-  expect(actual).toBe(expected);
-});
+test.each([
+  ['before.json', 'after.json', 'expected'],
+  ['before.yml', 'after.yml', 'expected'],
+  ['before.ini', 'after.ini', 'expected'],
+])(
+  'gendiff(%s, %s)',
+  (first, second, expected) => {
+    const pathOfTests = '__tests__/__fixtures__/';
+    expect(gendiff(`${pathOfTests}${first}`, `${pathOfTests}${second}`))
+      .toBe(fs.readFileSync(`${pathOfTests}${expected}`, 'utf-8').trim());
+  },
+);
